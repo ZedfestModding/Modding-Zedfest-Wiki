3f854cc65efa5218358dbab3af1587cf1cf0c5b0
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2021-06-26 14:43:37 -0400

    Updated Export Batch
    Changed export location of file map index

--- Modding Zedfest/Unpack + Umodel Extraction.bat
+++ Modding Zedfest/Unpack + Umodel Extraction.bat
@@ -1,4 +1,4 @@
-"UnrealPak Files\UnrealPak.exe" "..\Zedfest Pak Files" -GeneratePIXMappingFile -OutputPath="..\Modding Workspace"
+"UnrealPak Files\UnrealPak.exe" "..\Zedfest Pak Files" -GeneratePIXMappingFile -OutputPath="..\File Index"
 "UnrealPak Files\UnrealPak.exe" "Zedfest Pak Files\KevinSpel-WindowsNoEditor.pak" -Extract "..\Unpacking Export Folder"
 del /S *MessageBox3Ingame*
 "Umodel\umodel.exe" -path="Unpacking Export Folder" -game=ue4.22 -out="Umodel Export Folder" -sounds -export *.uasset
