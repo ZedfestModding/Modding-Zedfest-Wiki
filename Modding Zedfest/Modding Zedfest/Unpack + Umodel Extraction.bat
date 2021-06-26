42ad99c82b959df1e28c8bceaebe89d2ce4c0374
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2021-06-26 14:42:51 -0400

    Delete Modding Zedfest/Modding Zedfest directory

--- Modding Zedfest/Modding Zedfest/Unpack + Umodel Extraction.bat
+++
@@ -1,4 +0,0 @@
-"UnrealPak Files\UnrealPak.exe" "..\Zedfest Pak Files" -GeneratePIXMappingFile -OutputPath="..\File Index"
-"UnrealPak Files\UnrealPak.exe" "Zedfest Pak Files\KevinSpel-WindowsNoEditor.pak" -Extract "..\Unpacking Export Folder"
-del /S *MessageBox3Ingame*
-"Umodel\umodel.exe" -path="Unpacking Export Folder" -game=ue4.22 -out="Umodel Export Folder" -sounds -export *.uasset
